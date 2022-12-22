54c6f3e134bb85538c41879d1a90aa8aaadfa9e8
Sabin Marcu <sabinmarcu@gmail.com>
2022-12-22 12:26:15 +0200

    fix(root): fix moize global mock

--- .config/jest/setupFiles/moize.mjs
+++ .config/jest/setupFiles/moize.mjs
@@ -1,2 +1,7 @@
-// eslint-disable-next-line no-undef
-jest.mock('moize', jest.requireActual('../mocks/moize'));
+/* eslint-disable no-undef */
+jest.mock('moize', () => {
+  const moizeMock = jest.requireActual('../mocks/moize').mock;
+  const moizeActual = jest.requireActual('moize');
+  const patchedMoize = moizeMock(moizeActual);
+  return patchedMoize;
+});
