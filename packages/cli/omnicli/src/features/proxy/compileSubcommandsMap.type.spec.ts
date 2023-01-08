bb91fbc689e102382a16ce239295d2ea4ebe59c7
Sabin Marcu <sabinmarcu@gmail.com>
2023-01-08 12:12:48 +0200

    test(omnicli): refresh type spec test

--- packages/cli/omnicli/src/features/proxy/compileSubcommandsMap.type.spec.ts
+++ packages/cli/omnicli/src/features/proxy/compileSubcommandsMap.type.spec.ts
@@ -9,16 +9,21 @@ type MapOfSubcommandsListTest = MapOfSubcommandsList<typeof testSubcommands>;
 //           test1: typeof Test1;
 //           test2: typeof Test2;
 //           test3: typeof Test2;
+//           test4: typeof Test3;
+//           "test5:test6": typeof Test3;
 //       }
 
 const compileSubcommandsMapTest = compileSubcommandsMap(testSubcommands);
-//    ^? const compileSubcommandsMapTest: MapOfSubcommandsList<readonly [typeof Test1, typeof Test2], {
+//    ^? const compileSubcommandsMapTest: MapOfSubcommandsList<readonly [typeof Test1, typeof Test2, typeof Test3], {
 //           item: typeof Test1;
 //           paths: "test1";
 //       } | {
 //           item: typeof Test2;
 //           paths: "test2" | "test3";
+//       } | {
+//           item: typeof Test3;
+//           paths: "test4" | "test5:test6";
 //       }>
 
 type keysOfCompileSubcommandsMapTest = keyof typeof compileSubcommandsMapTest;
-//    ^? type keysOfCompileSubcommandsMapTest = "test1" | "test2" | "test3"
+//    ^? type keysOfCompileSubcommandsMapTest = "test1" | "test2" | "test3" | "test4" | "test5:test6"
