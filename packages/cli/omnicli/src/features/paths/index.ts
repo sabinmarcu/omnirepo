3b0a4850cd696e18131fe4074feec10d3dad7509
Sabin Marcu <sabinmarcu@gmail.com>
2023-01-08 11:12:24 +0200

    fix(omnicli): implement unpackSinglePath

--- packages/cli/omnicli/src/features/paths/index.ts
+++ packages/cli/omnicli/src/features/paths/index.ts
@@ -1,3 +1,4 @@
 export * from './types';
 export { getSinglePathOf } from './getSinglePathOf';
 export { getSinglePathsOf } from './getSinglePathsOf';
+export { unpackSinglePath } from './unpackSinglePath';
