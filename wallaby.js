e227719644c06e1d74c2e460044eb897e107df51
Sabin Marcu <sabinmarcu@gmail.com>
2023-02-09 21:58:48 +0200

    feat(root): improve coverage reporting

--- wallaby.js
+++ wallaby.js
@@ -1,3 +1,4 @@
+import { coverageExcludes } from './jest.options.mjs';
 import packageJson from './package.json' assert { type: 'json' };
 
 const files = packageJson.workspaces.map(
@@ -8,17 +9,14 @@ const tests = packageJson.workspaces.map(
   (workspace) => `${workspace}/src/**/!(*.type).spec.{ts,tsx}`,
 );
 
+const filesWithNoCoverageCalculated = coverageExcludes.map(
+  (it) => it.replace('!<rootDir>/', ''),
+);
+
 export default () => ({
   name: 'omniRepo',
   files,
   tests,
   autoDetect: true,
-  filesWithNoCoverageCalculated: [
-    'config/eslint-config/src/configs/**/*',
-    'utils/utils-test/src/**/*',
-    '**/index.ts',
-    '**/types.ts',
-    '**/constants.ts',
-    '**/*.d.ts',
-  ],
+  filesWithNoCoverageCalculated,
 });
