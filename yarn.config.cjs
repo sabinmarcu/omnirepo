09a1363fb18c3bc705df4866d11f4fed16a61517
Sabin Marcu <sabinmarcu@gmail.com>
2023-11-11 00:24:00 +0200

    feat(root): update yarn.config.cjs with eslint rules

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -1,4 +1,5 @@
-// @ts-check 
+// @ts-check
+/* eslint-disable no-continue */
 
 /**
  * @typedef {import('@yarnpkg/types').Yarn.Constraints.Workspace} Workspace
@@ -8,12 +9,13 @@
 
 /** @type {import('@yarnpkg/types')} */
 const { defineConfig } = require('@yarnpkg/types');
-const WORKSPACE_PROTOCOL_RANGE = "workspace:*"
-const FIELD_IGNORE_LIST = ['root']
+
+const WORKSPACE_PROTOCOL_RANGE = 'workspace:*';
+const FIELD_IGNORE_LIST = new Set(['root']);
 const FIELD_UPDATE_MAP = {
-  'type': 'module',
-  'main': './cjs/index.cjs',
-  'module': './esm/index.mjs',
+  type: 'module',
+  main: './cjs/index.cjs',
+  module: './esm/index.mjs',
   'exports.["."].require': './cjs/index.cjs',
   'exports.["."].import': './esm/index.mjs',
   'exports.["./src/*"].require': './src/*',
@@ -21,10 +23,10 @@ const FIELD_UPDATE_MAP = {
   'exports.["./*"].require': './cjs/*.cjs',
   'exports.["./*"].import': './esm/*.mjs',
   'exports.["./package.json"]': './package.json',
-  'typeVersions.["*"]["*"][0]': "./esm/*",
-  "build.preset": "../../../.config/build.config.ts",
-  "tscmono.preset": "lib",
-}
+  'typeVersions.["*"]["*"][0]': './esm/*',
+  'build.preset': '../../../.config/build.config.ts',
+  'tscmono.preset': 'lib',
+};
 
 /**
  * Ensure that all workspace dependencies are using workspace protocol
@@ -33,8 +35,11 @@ const FIELD_UPDATE_MAP = {
  */
 async function ensureWorkspaceProtocol({ Yarn }) {
   for (const dependency of Yarn.dependencies()) {
-    if (Yarn.workspace({ ident: dependency.ident }) && dependency.range !== WORKSPACE_PROTOCOL_RANGE) {
-      dependency.update(WORKSPACE_PROTOCOL_RANGE)
+    if (
+      Yarn.workspace({ ident: dependency.ident })
+      && dependency.range !== WORKSPACE_PROTOCOL_RANGE
+    ) {
+      dependency.update(WORKSPACE_PROTOCOL_RANGE);
     }
   }
 }
@@ -42,17 +47,17 @@ async function ensureWorkspaceProtocol({ Yarn }) {
 /**
  * This rule will enforce that a workspace MUST depend on the same version of
  * a dependency as the one used by the other workspaces.
- * 
+ *
  * @param {Context} context
  */
 function enforceConsistentDependenciesAcrossTheProject({ Yarn }) {
   for (const dependency of Yarn.dependencies()) {
-    if (dependency.type === `peerDependencies`) {
+    if (dependency.type === 'peerDependencies') {
       continue;
     }
 
     for (const otherDependency of Yarn.dependencies({ ident: dependency.ident })) {
-      if (otherDependency.type === `peerDependencies`) {
+      if (otherDependency.type === 'peerDependencies') {
         continue;
       }
 
@@ -68,7 +73,7 @@ function enforceConsistentDependenciesAcrossTheProject({ Yarn }) {
  */
 async function ensureFieldConsistency({ Yarn }) {
   for (const workspace of Yarn.workspaces()) {
-    if (FIELD_IGNORE_LIST.includes(`${workspace.ident}`)) {
+    if (FIELD_IGNORE_LIST.has(`${workspace.ident}`)) {
       continue;
     }
     for (const [field, value] of Object.entries(FIELD_UPDATE_MAP)) {
@@ -89,5 +94,5 @@ async function constraints(context) {
 }
 
 module.exports = defineConfig({
-  constraints
-})
+  constraints,
+});
