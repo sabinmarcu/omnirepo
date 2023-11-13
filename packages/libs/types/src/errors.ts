810ebb4a5bdb3f68b45b01051655523513ea66fc
Sabin Marcu <sabinmarcu@gmail.com>
2023-11-13 05:56:12 +0200

    feat(types): improve TypeError to enable use as extends

--- packages/libs/types/src/errors.ts
+++ packages/libs/types/src/errors.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/naming-convention,no-underscore-dangle
 const __TypeError__ = Symbol('TypeError');
 
-export type TypeError<Message extends string> = { [__TypeError__]: Message };
+export type TypeError<Message extends string = string> = { [__TypeError__]: Message };
