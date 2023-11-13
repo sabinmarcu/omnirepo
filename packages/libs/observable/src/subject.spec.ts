20642073bfccf2436689b5d0ee283893115e69c0
Sabin Marcu <sabinmarcu@gmail.com>
2023-11-13 10:19:30 +0200

    fix(observable): implement PipedObservable back into subject and write tests

---
+++ packages/libs/observable/src/subject.spec.ts
@@ -0,0 +1,77 @@
+import { mock } from './mock';
+import {
+  isObservable,
+} from './observable';
+import { subject } from './subject';
+
+describe('subject', () => {
+  it('should be a function', () => {
+    expect(typeof subject).toBe('function');
+  });
+  it('should have one argument', () => {
+    expect(subject.length).toBe(1);
+  });
+  it('should return an observable', () => {
+    expect(isObservable(subject())).toBe(true);
+  });
+  it('should update value when changed', () => {
+    const initialValue = 42;
+    const subj = subject(initialValue);
+    expect(subj.value).toBe(initialValue);
+    subj.next(initialValue * 2);
+    expect(subj.value).toBe(initialValue * 2);
+  });
+  it('should respond to subcribers value when changed', () => {
+    const initialValue = 42;
+    const subj = subject(initialValue);
+    const next = jest.fn();
+    const sub = subj.subscribe({ next });
+    expect(subj.value).toBe(initialValue);
+    expect(next).toHaveBeenCalledWith(initialValue);
+    expect(next).toHaveBeenCalledTimes(1);
+    subj.next(initialValue * 2);
+    expect(subj.value).toBe(initialValue * 2);
+    expect(next).toHaveBeenCalledWith(initialValue * 2);
+    expect(next).toHaveBeenCalledTimes(2);
+    sub.unsubscribe();
+  });
+  it('should map correctly to double value', () => {
+    const initialValue = 42;
+    const subj = subject(initialValue);
+    const obs = subj.map((v) => v * 2);
+    expect(subj.value).toBe(initialValue);
+    expect(obs.value).toBe(initialValue * 2);
+  });
+
+  describe('subscription pool (mocking)', () => {
+    describe('subscription pool', () => {
+      it('should be empty by default', () => {
+        expect(mock.subscriptionPool.size).toBe(0);
+      });
+      it('should increase when subscribing', () => {
+        const obs = subject();
+        expect(mock.subscriptionPool.size).toBe(0);
+        const sub = obs.subscribe({} as any);
+        expect(mock.subscriptionPool.size).toBe(1);
+        sub.unsubscribe();
+        expect(mock.subscriptionPool.size).toBe(0);
+      });
+      it('should empty with the correct method', () => {
+        const obs = subject();
+        expect(mock.subscriptionPool.size).toBe(0);
+        const next = jest.fn();
+        obs.subscribe({ next });
+        expect(next).toHaveBeenCalledWith(undefined);
+        expect(next).toHaveBeenCalledTimes(1);
+        obs.next(42);
+        expect(next).toHaveBeenCalledWith(42);
+        expect(next).toHaveBeenCalledTimes(2);
+        expect(mock.subscriptionPool.size).toBe(1);
+        mock.emptySubscriptionPool();
+        expect(mock.subscriptionPool.size).toBe(0);
+        obs.next(69);
+        expect(next).toHaveBeenCalledTimes(2);
+      });
+    });
+  });
+});
