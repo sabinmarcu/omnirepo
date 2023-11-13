f053e28a9fb7e164066f8a78ecf8afc01e64ee58
Sabin Marcu <sabinmarcu@gmail.com>
2023-11-13 10:19:30 +0200

    fix(observable): implement PipedObservable back into subject and write tests

--- packages/libs/observable/src/subject.ts
+++ packages/libs/observable/src/subject.ts
@@ -4,15 +4,21 @@ import type {
   Subject,
 } from './types';
 
-export const subject = <T>() => {
+export const subject = <T>(initialValue?: T) => {
   let controller: ObserverController<T>;
 
-  const root = observable<T>((observableController) => {
-    controller = observableController;
-  });
+  const root = observable<T>(
+    (observableController) => {
+      controller = observableController;
+    },
+    initialValue,
+  );
 
   return {
     ...root,
+    get value() {
+      return root.value;
+    },
     // @ts-ignore
     ...controller,
   } satisfies Subject<T>;
