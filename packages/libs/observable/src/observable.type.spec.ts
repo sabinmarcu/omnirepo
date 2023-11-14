ad4e8df9fbedc771fa024e9bc6cf814ff49486cc
Sabin Marcu <sabinmarcu@gmail.com>
2023-11-14 15:07:21 +0200

    feat(observable): update observables to always be optionally undefined

--- packages/libs/observable/src/observable.type.spec.ts
+++ packages/libs/observable/src/observable.type.spec.ts
@@ -5,19 +5,19 @@ const testObservableNumber = observable.from(42);
 const testObservableString = observable.from('Value: ');
 
 const testObservableValue = testObservableNumber.value;
-//    ^? const testObservableValue: 42
+//    ^? const testObservableValue: 42 | undefined
 
 const testObservableSubscribe = testObservableNumber.subscribe;
-//    ^? const testObservableSubscribe: (observer: Observer<42>) => Subscription
+//    ^? const testObservableSubscribe: (observer: Observer<42 | undefined>) => Subscription
 
 const testObservableSubscription = testObservableNumber.subscribe({} as any);
 //    ^? const testObservableSubscription: Subscription
 
 const testObservableFilter = testObservableNumber.filter;
-//    ^? const testObservableFilter: ObservableFilter<42>
+//    ^? const testObservableFilter: ObservableFilterFunction<42>
 
 const testObservableMap = testObservableNumber.map;
-//    ^? const testObservableMap: ObservableMap<42>
+//    ^? const testObservableMap: ObservableMapFunction<42>
 
 const testObservableFrom = observable.from(1);
 //    ^? const testObservableFrom: Observable<1>
@@ -33,4 +33,4 @@ const testObservableProjectionValue = testObservableProjection.value;
 //           prefix: "Value: ";
 //           value: 42;
 //           result: string;
-//       }
+//       } | undefined
