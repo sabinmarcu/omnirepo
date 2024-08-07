8e6236d0735d4354439ea85e791dfd01f6cce0a1
Sabin Marcu <sabin.marcu@platform24.com>
2024-08-07 15:44:06 +0300

    feat(use-local-storage): implement useLocalStorage hook

---
+++ workspaces/hooks/useLocalStorage/src/useLocalStorage.ts
@@ -0,0 +1,80 @@
+import {
+  useCallback,
+  useEffect,
+  useState,
+  useMemo,
+  useDebugValue,
+} from 'react';
+import type {
+  Dispatch,
+  SetStateAction,
+} from 'react';
+
+export const useLocalStorage = <T extends unknown>(
+  key: string,
+  initialValue: T,
+) => {
+  const getValueFromLocalStorage = useCallback(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch {
+      return initialValue;
+    }
+  }, [
+    initialValue,
+    key,
+  ]);
+
+  const [
+    storedValue,
+    setStoredValue,
+  ] = useState<T>(getValueFromLocalStorage());
+
+  useEffect(() => {
+    const value = getValueFromLocalStorage();
+    setStoredValue(value);
+  }, [getValueFromLocalStorage]);
+
+  const changeHandler = useCallback(
+    (event: StorageEvent) => {
+      const {
+        key: changeKey, newValue,
+      } = event;
+      if (key === changeKey) {
+        setStoredValue(JSON.parse(newValue || ''));
+      }
+    },
+    [key],
+  );
+
+  useEffect(() => {
+    window.addEventListener('storage', changeHandler);
+    return () => {
+      window.removeEventListener('storage', changeHandler);
+    };
+  }, [changeHandler]);
+
+  useDebugValue(`${key}: ${storedValue}`);
+
+  return useMemo(() => {
+    const setValue = ((value: SetStateAction<T>) => {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      if (valueToStore !== storedValue) {
+        setStoredValue(valueToStore);
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+      return true;
+    }) satisfies Dispatch<SetStateAction<T>>;
+
+    return [
+      storedValue,
+      setValue,
+    ] as const;
+  }, [
+    storedValue,
+    key,
+  ]);
+};
+
+export default useLocalStorage;
