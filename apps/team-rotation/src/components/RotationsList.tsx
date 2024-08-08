96e43128f106a3b927b9eb808cff9d44634443d8
Sabin Marcu <sabin.marcu@platform24.com>
2024-08-08 18:43:06 +0300

    feat(team-rotation): implement adding and removing of new rotations

--- apps/team-rotation/src/components/RotationsList.tsx
+++ apps/team-rotation/src/components/RotationsList.tsx
@@ -1,19 +1,57 @@
 import {
   useAtom,
+  useAtomValue,
 } from 'jotai/react';
+import { PlusOne } from '@mui/icons-material';
+import { useCallback } from 'react';
 import {
+  rotationsAtom,
   rotationsListAtom,
 } from '../state/atoms.ts';
-import { RotationsListWrapper } from './RotationsList.style.tsx';
+import {
+  RotationListAddButton,
+  RotationsListWrapper,
+} from './RotationsList.style.tsx';
 import { Rotation } from './Rotation.tsx';
+import { generateRotation } from '../state/seed.ts';
+import type { RotationType } from '../state/types.ts';
 
 export function RotationsList() {
-  const [rotations] = useAtom(rotationsListAtom);
+  const [
+    rotationsAtoms,
+    setRotations,
+  ] = useAtom(rotationsAtom);
+  const rotations = useAtomValue(rotationsListAtom);
+  const addRotation = useCallback(
+    () => setRotations(
+      (previousRotations) => [
+        ...previousRotations,
+        generateRotation(),
+      ],
+    ),
+    [setRotations],
+  );
+  const removeRotation = useCallback(
+    (rotation: RotationType) => () => {
+      setRotations((oldRotations) => {
+        const newRotations = oldRotations.filter((maybeRotation) => rotation !== maybeRotation);
+        return newRotations;
+      });
+    },
+    [setRotations],
+  );
   return (
     <RotationsListWrapper>
-      {rotations.map((atom) => (
-        <Rotation atom={atom} key={`${atom}`} />
+      {rotations.map((atom, index) => (
+        <Rotation
+          atom={atom}
+          key={JSON.stringify(rotationsAtoms[index])}
+          onRemove={removeRotation(rotationsAtoms[index])}
+        />
       ))}
+      <RotationListAddButton onClick={addRotation}>
+        <PlusOne />
+      </RotationListAddButton>
     </RotationsListWrapper>
   );
 }
