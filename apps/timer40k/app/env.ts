3313ad3ebe3f04c1e6995e7f556c22f84aec09d4
Sabin Marcu <sabinmarcu@gmail.com>
2024-08-11 20:26:18 +0000

    feat(timer40k): add game title to env

--- apps/timer40k/app/env.ts
+++ apps/timer40k/app/env.ts
@@ -2,9 +2,11 @@ import { z } from 'zod';
 
 const envSchema = z.object({
   TIMER_40K_RELEASE_DATE: z.string(),
+  TIMER_40K_GAME_NAME: z.string(),
 });
 
 export type EnvType = z.infer<typeof envSchema>;
 export const env = envSchema.parse({
   TIMER_40K_RELEASE_DATE: process.env.TIMER_40K_RELEASE_DATE,
+  TIMER_40K_GAME_NAME: process.env.TIMER_40K_GAME_NAME,
 });
--- apps/timer40k/app/layout.tsx
+++ apps/timer40k/app/layout.tsx
@@ -3,12 +3,15 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './index.css';
 import './globals.css';
+import { env } from './env';
 
 const inter = Inter({ subsets: ['latin'] });
 
+const gameName = env.TIMER_40K_GAME_NAME ?? "Unknown Game";
+
 export const metadata: Metadata = {
-  title: 'Is Darktide out yet?',
-  description: 'Timer until Darktide is out',
+  title: `Is ${gameName} out yet?`,
+  description: `Timer until ${gameName} is out`,
 };
 
 export default function RootLayout({
