739e97e7ddd9357b070bf85df171601b55cfcf79
Sabin Marcu <sabin.marcu@platform24.com>
2024-09-01 03:33:40 +0300

    feat(colors): implement hex types

---
+++ workspaces/design-system/colors/src/types/hex.ts
@@ -0,0 +1,50 @@
+import type {
+  IsNever,
+  MatchRepeat,
+  Repeats,
+  RepeatsType,
+} from '@sabinmarcu/types';
+
+export type HexCharacters = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' | 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'A' | 'B' | 'C' | 'D' | 'E' | 'F';
+export type HexMatchSingular = HexCharacters;
+export type HexMatchDouble = `${HexCharacters}${HexCharacters}`;
+
+export type HexColorType = `#${string}`;
+export type HexColorStringOf<T extends string> = (
+  T extends `#${infer Value}` ? Value : never
+);
+export type HexColorOf<T extends string> = `#${T}`;
+
+export type HexStringMatch<
+  T extends string,
+  Repetitions extends RepeatsType,
+  HexSingle = MatchRepeat<T, HexMatchSingular, Repetitions>,
+  HexDouble = MatchRepeat<T, HexMatchDouble, Repetitions>,
+> = (
+  IsNever<HexSingle> extends true
+    ? IsNever<HexDouble> extends true
+      ? never
+      : HexDouble
+    : HexSingle
+);
+
+export type HexString<
+  T extends string,
+  HexStringValue = HexStringMatch<T, Repeats<3>>,
+  HexAlphaStringValue = HexStringMatch<T, Repeats<4>>,
+> = (
+  IsNever<HexStringValue> extends true
+    ? IsNever<HexAlphaStringValue> extends true
+      ? never
+      : HexAlphaStringValue
+    : HexStringValue
+);
+
+export type HexColor<
+  T extends string,
+  HexValue = HexString<HexColorStringOf<T>>,
+> = (
+  IsNever<HexValue> extends false
+    ? T
+    : never
+);
---
+++ workspaces/design-system/colors/src/types/hex.type.spec.ts
@@ -0,0 +1,60 @@
+import type { Repeats } from '@sabinmarcu/types';
+import type {
+  HexColor,
+  HexColorOf,
+  HexColorStringOf,
+  HexString,
+  HexStringMatch,
+} from './hex.js';
+
+type HexColorValueOfTest1 = HexColorStringOf<'#fff'>;
+//    ^? type HexColorValueOfTest1 = "fff"
+
+type HexColorValueOfTest2 = HexColorStringOf<'#ffffff'>;
+//    ^? type HexColorValueOfTest2 = "ffffff"
+
+type HexColorValueOfTest3 = HexColorStringOf<'#0cf'>;
+//    ^? type HexColorValueOfTest3 = "0cf"
+
+type HexColorValueOfTest4 = HexColorStringOf<'#0cf8'>;
+//    ^? type HexColorValueOfTest4 = "0cf8"
+
+type HexColorOfTest1 = HexColorOf<'fff'>;
+//    ^? type HexColorOfTest1 = "#fff"
+
+type HexStringMatchTest1 = HexStringMatch<'fff', Repeats<3>>;
+//    ^? type HexStringMatchTest1 = "fff"
+
+type HexStringMatchTest2 = HexStringMatch<'ffffff', Repeats<3>>;
+//    ^? type HexStringMatchTest2 = "ffffff"
+
+type HexStringMatchTest3 = HexStringMatch<'0cf', Repeats<3>>;
+//    ^? type HexStringMatchTest3 = "0cf"
+
+type HexStringTest1 = HexString<'fff'>;
+//    ^? type HexStringTest1 = "fff"
+
+type HexStringTest2 = HexString<'ffff'>;
+//    ^? type HexStringTest2 = "ffff"
+
+type HexStringTest3 = HexString<'ffffff'>;
+//    ^? type HexStringTest3 = "ffffff"
+
+type HexStringTest4 = HexString<'ffffffff'>;
+//    ^? type HexStringTest4 = "ffffffff"
+
+type HexColorTest1 = HexColor<'#fff'>;
+//    ^? type HexColorTest1 = "#fff"
+
+type HexColorTest2 = HexColor<'#ffff'>;
+//    ^? type HexColorTest2 = "#ffff"
+
+type HexColorTest3 = HexColor<'#ffffff'>;
+//    ^? type HexColorTest3 = "#ffffff"
+
+type HexColorTest4 = HexColor<'#ffffffff'>;
+//    ^? type HexColorTest4 = "#ffffffff"
+
+type HexColorTest5 = HexColor<'#0cfg'>;
+//    ^? type HexColorTest5 = never
+
