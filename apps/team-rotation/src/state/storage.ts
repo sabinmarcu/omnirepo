42d6a5d54d79aca0fd265aa06ab0756f0c816d00
Sabin Marcu <sabin.marcu@platform24.com>
2025-01-20 11:05:35 +0200

    feat(team-rotation): add pako support
    opt for pako encoding for querystring state but still support migration
    from b64

--- apps/team-rotation/src/state/storage.ts
+++ apps/team-rotation/src/state/storage.ts
@@ -1,12 +1,28 @@
 import {
-  b64EncoderFromSchema,
+  b64Encoder,
+  pakoEncoder,
   queryStorage,
 } from '@sabinmarcu/jotai-storage-adapter-querystring';
 import { atomWithStorage } from 'jotai/utils';
-import { seedData } from './seed.ts';
-import { stateSchema } from './schema.ts';
+import type { Encoder } from '@sabinmarcu/jotai-storage-adapter-querystring/types';
+import type { z } from 'zod';
+import { seedData } from './seed.js';
+import { stateSchema } from './schema.js';
 
-const storage = queryStorage(b64EncoderFromSchema(stateSchema));
+const adaptableEncoder = {
+  encode: pakoEncoder.encode,
+  decode: (input: string) => {
+    let jsonInput: unknown;
+    try {
+      jsonInput = pakoEncoder.decode(input);
+    } catch {
+      jsonInput = b64Encoder.decode(input);
+    }
+    return stateSchema.parse(jsonInput);
+  },
+} as const satisfies Encoder<z.infer<typeof stateSchema>>;
+
+const storage = queryStorage(adaptableEncoder);
 export const storageAtom = atomWithStorage(
   'state',
   storage.getItem('state', seedData),
