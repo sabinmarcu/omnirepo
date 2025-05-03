7f1c804c4bf25e00d96d1b0d063889988a34643f
Sabin Marcu <sabin.marcu@platform24.com>
2025-05-03 07:31:22 +0300

    feat(theme-storybook): move storybook theme setup to this package

--- workspaces/design-system/theme-storybook/src/types.ts
+++ workspaces/design-system/theme-storybook/src/types.ts
@@ -1,6 +1,9 @@
+import type { PartialDeep } from '@sabinmarcu/types';
 import type { Preview } from '@storybook/react';
+import type { StorybookConfig } from '@storybook/react-vite';
 
 export type Extension = {
   manager?: () => void;
   preview?: Preview,
+  managerObject?: PartialDeep<StorybookConfig>,
 };
