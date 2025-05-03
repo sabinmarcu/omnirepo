fc9a0d31a2fb9621feefedbc7cb254294f60a923
Sabin Marcu <sabin.marcu@platform24.com>
2025-05-03 14:26:59 +0300

    feat(theme): add support for switching family through data-attribute

--- workspaces/design-system/theme/src/contracts/theme.stories.css.ts
+++ workspaces/design-system/theme/src/contracts/theme.stories.css.ts
@@ -6,11 +6,37 @@ import {
   setupTheme,
   ThemeMetadataSymbol,
 } from './theme.js';
+import {
+  themeDataAttribute,
+  themeFamilyDataAttribute,
+} from '../constants.js';
+
+const familyDataAttribute = `data-${themeFamilyDataAttribute}`;
+const variantDataAttribute = `data-${themeDataAttribute}`;
 
 const { contract: themeContract } = setupTheme[ThemeMetadataSymbol];
+
 export const swatchSet = style({
   display: 'grid',
   gridTemplateColumns: 'minmax(5cqw, 10cqw) auto',
+  selectors: {
+    [`&[${familyDataAttribute}]::after`]: {
+      gridRow: 2,
+      gridColumn: '1/3',
+      content: `"[${familyDataAttribute}="attr(${familyDataAttribute})"]"`,
+      display: 'grid',
+      placeItems: 'end',
+      opacity: 0.3,
+    },
+    [`&[${variantDataAttribute}]::before`]: {
+      gridRow: 2,
+      gridColumn: '1/3',
+      content: `"[${variantDataAttribute}="attr(${variantDataAttribute})"]"`,
+      display: 'grid',
+      placeItems: 'end',
+      opacity: 0.3,
+    },
+  },
 });
 
 globalStyle(`${swatchSet} > *:first-child`, {
