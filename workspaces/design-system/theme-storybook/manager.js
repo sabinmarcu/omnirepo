ed587b5d46d185729eb5391d7a497980676d7546
Sabin Marcu <sabin.marcu@platform24.com>
2025-05-03 07:17:22 +0300

    feat(theme-storybook): update tsconfig to classic react (thanks, storybook)

--- workspaces/design-system/theme-storybook/manager.js
+++ workspaces/design-system/theme-storybook/manager.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-commonjs */
 /* eslint-disable unicorn/prefer-module */
 /* eslint-disable import/extensions */
-require('@sabinmarcu/storybook-addon-split-toolbars/manager');
 module.exports = require('./dist/manager.js');
--- workspaces/design-system/theme-storybook/src/manager.ts
+++ workspaces/design-system/theme-storybook/src/manager.ts
@@ -1,3 +1,7 @@
+import '@sabinmarcu/storybook-addon-split-toolbars/manager';
+import '@sabinmarcu/storybook-addon-theme-overrider/manager';
+import '@sabinmarcu/storybook-addon-mirror-preview/manager';
+
 import { extensions } from './extensions/index.js';
 
 for (const extension of extensions) {
