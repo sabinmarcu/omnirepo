78ca3c8dd97bfe9940893f95e99a7498f51bb798
Sabin Marcu <sabin.marcu@platform24.com>
2025-05-05 08:21:54 +0300

    feat(theme-storybook): update with website theme

--- workspaces/design-system/theme-storybook/src/extensions/themeFamily.tsx
+++ workspaces/design-system/theme-storybook/src/extensions/themeFamily.tsx
@@ -15,23 +15,20 @@ import {
   useGlobals,
 } from '@storybook/manager-api';
 import { splitToolbar } from '@sabinmarcu/storybook-addon-split-toolbars';
-import { themes } from '../config/themes.js';
 import type { Extension } from '../types.js';
 import { THEME_SELECTOR_GLOBAL_ID } from './themeVariant.js';
-
-const themeSelectionList = themes.families.map((family) => ({
-  value: family,
-  title: `${family[0].toUpperCase()}${family.slice(1)} Theme`,
-})) as {
-  value: typeof themes['families'][number],
-  title: string,
-}[];
+import { themeMapping } from './themeFamily.data.js';
 
 export const THEME_FAMILY_GLOBAL_ID = 'themeFamily';
 export const THEME_FAMILY_ADDON_ID = `${THEME_FAMILY_GLOBAL_ID}.addon`;
 export const THEME_FAMILY_TOOL_ID = `${THEME_FAMILY_GLOBAL_ID}.tool`;
 
 const dataAttribute = `data-${themeFamilyDataAttribute}`;
+
+const mappingGroups = Object.keys(themeMapping);
+const isValidMapping = (input: string): input is keyof typeof themeMapping => (
+  mappingGroups.includes(input)
+);
 
 type ThemeFamilySelectorProps = PropsWithChildren<{
   globals: Globals
@@ -40,10 +37,15 @@ const ThemeFamilySelector = memo(({ globals, children }: ThemeFamilySelectorProp
   useEffect(
     () => {
       const { [THEME_FAMILY_GLOBAL_ID]: selection } = globals;
+      if (!selection) return;
+      const [group, theme] = selection.split(':');
       const target = document.querySelector(rootNode);
       if (!target) return;
-      if (themes.families.includes(selection)) {
-        target.setAttribute(dataAttribute, selection);
+      if (
+        isValidMapping(group)
+        && themeMapping[group].list.some(({ value }: any) => value === theme)
+      ) {
+        target.setAttribute(dataAttribute, theme);
       } else {
         target.removeAttribute(dataAttribute);
       }
@@ -83,12 +85,12 @@ const themeFamilySelectorToolbar: Extension['preview'] = {
         icon: 'chromatic',
         dynamicTitle: true,
         title: 'Theme Family',
-        items: themeSelectionList,
+        items: themeMapping,
       },
     }),
   },
   initialGlobals: {
-    [THEME_FAMILY_GLOBAL_ID]: 'base',
+    [THEME_FAMILY_GLOBAL_ID]: 'playground:base',
   },
   decorators: [ThemeFamilySelectorDecorator],
 };
