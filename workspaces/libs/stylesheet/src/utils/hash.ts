339dcb1e05cb27db800b8eea560c0d43a323adf3
Sabin Marcu <sabinmarcu@gmail.com>
2026-08-11 18:08:10 +0300

    refactor(stylesheet): comply with updated lint rules

--- workspaces/libs/stylesheet/src/utils/hash.ts
+++ workspaces/libs/stylesheet/src/utils/hash.ts
@@ -1,11 +1,12 @@
 const hashCharacters = [
-  Array.from({ length: 26 }).map((_, index) => 65 + index),
-  Array.from({ length: 26 }).map((_, index) => 97 + index),
-  Array.from({ length: 10 }).map((_, index) => index),
+  Array.from({ length: 26 }, (_, index) => 65 + index),
+  Array.from({ length: 26 }, (_, index) => 97 + index),
+  Array.from({ length: 10 }, (_, index) => index),
 ].flat();
 
 export const makeHash = (length = 12) => (
-  Array.from({ length }).fill(() => hashCharacters[
-    Math.floor(Math.random() * hashCharacters.length)
-  ]).join('')
+  Array.from(
+    { length },
+    () => hashCharacters[Math.floor(Math.random() * hashCharacters.length)],
+  ).join('')
 );
