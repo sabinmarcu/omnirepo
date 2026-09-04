f5bf26ffcd5cc896889a76bf600cebb6521b8d18
Sabin Marcu <sabinmarcu@gmail.com>
2026-09-05 00:25:48 +0300

    fix(website): add distinct h4 styles for typography

--- apps/website/components/primitives/Typography.css.ts
+++ apps/website/components/primitives/Typography.css.ts
@@ -1,23 +1,49 @@
 import { theme } from '@sabinmarcu/theme';
-import { styleVariants } from '@vanilla-extract/css';
+import {
+  createVar,
+  globalStyle,
+  styleVariants,
+} from '@vanilla-extract/css';
 import { typographyUnstyledDataAttribute } from './Typography.constants';
 
+const variables = {
+  divider: {
+    size: createVar('typography-divider-size'),
+    color: createVar('typography-divider-color'),
+    spacing: createVar('typography-divider-spacing'),
+  },
+  spacing: {
+    start: createVar('typography-spacing-start'),
+    end: createVar('typography-spacing-end'),
+  },
+} as const;
+
+globalStyle('body > main', {
+  vars: {
+    [variables.divider.size]: '1px',
+    [variables.divider.color]: theme.colors.primary.muted,
+    [variables.divider.spacing]: theme.grid.m,
+    [variables.spacing.start]: theme.grid.xxl,
+    [variables.spacing.end]: theme.grid.s,
+  },
+});
+
 const headingBase = {
-  marginBlockStart: theme.grid.xxl,
-  marginBlockEnd: theme.grid.s,
+  marginBlockStart: variables.spacing.start,
+  marginBlockEnd: variables.spacing.end,
   lineHeight: 1,
 } as const;
 
 const dividerHeading = {
-  borderBlockStart: `1px solid ${theme.colors.primary.muted}`,
-  paddingBlockStart: theme.grid.m,
+  borderBlockStart: `${variables.divider.size} solid ${variables.divider.color}`,
+  paddingBlockStart: variables.divider.spacing,
 } as const;
 
 const whenStyled = <Style extends object>(style: Style) => ({
   selectors: {
     [`&:not([${typographyUnstyledDataAttribute}] *)`]: style,
   },
-});
+} as Style);
 
 export const typographyStyles = styleVariants({
   h1: whenStyled({
@@ -36,6 +62,7 @@ export const typographyStyles = styleVariants({
   h4: whenStyled({
     ...headingBase,
     ...dividerHeading,
+    borderBlockStartColor: `oklch(from ${variables.divider.color} l c h / 0.2)`,
     fontSize: theme.grid.l,
   }),
   h5: whenStyled({
