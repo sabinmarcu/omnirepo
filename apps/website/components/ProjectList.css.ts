d4b0f02f070a07c74213bd493daba38dcec3fa4c
Sabin Marcu <sabinmarcu@gmail.com>
2026-09-07 02:00:03 +0300

    fix(website): show project cards on mobile

--- apps/website/components/ProjectList.css.ts
+++ apps/website/components/ProjectList.css.ts
@@ -1,5 +1,9 @@
 import { style } from '@vanilla-extract/css';
+import { mobileMedia } from '@/utils/responsive';
 
 export const projectListStyle = style({
   alignItems: 'start',
+  ...mobileMedia({
+    alignItems: 'stretch',
+  }),
 });
